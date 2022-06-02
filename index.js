a260949d9c4b0fd04620dd6353c914426fdb0424
Maxwel <maxweloramat@gmail.com>
2022-06-02 04:16:48 -0400

    my commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats =["Milo" , "Otis" , "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+function appendCat(name){
+    const name1 = [...cats,name]
+    return name1;
+}
+function prependCat(name){
+    const pend = [name,...cats];
+    return pend;
+}
+function removeLastCat(){
+    const remove = cats.slice(0,cats.length-1);
+    return remove;
+}
+function removeFirstCat(){
+    const first = cats.slice(1);
+    return first;
+}
